41991fffc84fa1a4240856f5f5dbe6127826dcc4
Fager <fagerhu03@gmail.com>
2025-11-26 04:08:57 +0200

    them app with color

---
+++ lib/them/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // LIGHT MODE
+  static const Color lightBackground = Color(0xFFFCFBE8);
+  static const Color lightPatternOverlay = Color(0xFFE8E2C8);
+  static const Color lightText = Color(0xFF3D3A2F);
+  static const Color lightPrimaryButton = Color(0xFFD6A00F);
+  static const Color lightField = Color(0xFF7A8450);
+  static const Color lightFieldText = Colors.white;
+
+  // DARK MODE
+  static const Color darkBackground = Color(0xFF0B1D26);
+  static const Color darkPatternOverlay = Color(0xFF112B36);
+  static const Color darkText = Color(0xFFE8E2C8);
+  static const Color darkPrimaryButton = Color(0xFFD6A00F);
+  static const Color darkField = Color(0xFF1F3A3D);
+  static const Color darkFieldText = Color(0xFFE8E2C8);
+}
